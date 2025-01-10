dfab776812aa046ebb0339821e88160ddc83ce94
TomBschmns <tom.boschmans@student.odisee.be>
2025-01-10 16:52:58 +0100

    filter works

--- vite-project/src/components/ListGroup.jsx
+++ vite-project/src/components/ListGroup.jsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 
 function ListGroup() {
   const [list, setList] = useState([]);
+  const [search, setSearch] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const re = new RegExp(String.raw`${search}`, "g");
+
+  //console.log(list.filter((l) => re.exec(l.title)));
 
   async function getList() {
     const urlGET = "https://jsonplaceholder.typicode.com/posts";
@@ -14,23 +19,52 @@ function ListGroup() {
     setList(data);
   }
 
+  const handleInputChange = (e) => {
+    const searchTerm = e.target.value;
+    setSearch(searchTerm);
+    setFilteredList(list.filter((l) => re.exec(l.title)));
+  };
+
   useEffect(() => {
     getList();
   }, []);
+
   return (
     <div>
       <input
-        type="text"
+        type="search"
         id="searchName"
         className="searchName"
         name="searchName"
         placeholder="search post titles"
+        value={search}
+        onChange={handleInputChange}
       />
       <div id="wrapperList" className="wrapperList">
         <ul id="list" className="list">
-          {list.map((item) => (
-            <li key={item.id}>{item.title}</li>
-          ))}
+          {search == "" ? (
+            <>
+              {list.map((item) => (
+                <li key={item.id}>
+                  {item.title}
+                  <button id={item.id} className="buttenView">
+                    View
+                  </button>
+                </li>
+              ))}
+            </>
+          ) : (
+            <>
+              {filteredList.map((item) => (
+                <li key={item.id}>
+                  {item.title}
+                  <button id={item.id} className="buttenView">
+                    View
+                  </button>
+                </li>
+              ))}
+            </>
+          )}
         </ul>
       </div>
     </div>
